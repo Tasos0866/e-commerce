8d0917a0aaf40e951941c5541b2b851e3004ab9b
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2021-08-27 17:55:16 +0200

    The navbar no longer overlaps with the products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://cdn.shopify.com/s/files/1/1703/6727/products/tropicfeel-web-ecommerce-jungle-chip-grey-01.jpg?v=1628159134' },
@@ -9,9 +10,12 @@ const products = [
 ]
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
-            <Grid container justify="center" spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
                         <Product product={product} />
