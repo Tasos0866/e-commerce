a80dbab9b353609f892559153d50df35a72f674f
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2021-08-27 17:23:44 +0200

    Created the product component

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://cdn.shopify.com/s/files/1/1703/6727/products/tropicfeel-web-ecommerce-jungle-chip-grey-01.jpg?v=1628159134' },
+    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://thumbs.dreamstime.com/b/apple-macbook-pro-retina-open-tab-safari-which-shows-yahoo-web-page-varna-bulgaria-march-directly-front-view-inch-91718330.jpg' }
+]
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+}
+
+export default Products
