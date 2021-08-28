be2257cf83aeeb802de16f4f52d1b409b7425cd3
Tasos Tsoukalas <tasostsouk.tt@gmail.com>
2021-08-28 15:39:57 +0200

    Added the shipping country field in the checkout form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../Paymentform';
+import { commerce } from '../../../lib/commerce';
 
 const steps = ['Shipping address', 'Payment details']
 
-function Checkout() {
+function Checkout({ cart }) {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
+    const [checkoutToken, setCheckoutToken] = useState(null);
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+
+                setCheckoutToken(token);
+            } catch (error) {
+                
+            }
+        }
+
+        generateToken();
+    }, [cart]);
 
     const Confirmation = () => (
         <div>
@@ -18,7 +34,7 @@ function Checkout() {
     )
 
     const Form = () => activeStep === 0
-        ? <AddressForm />
+        ? <AddressForm checkoutToken={checkoutToken} />
         : <PaymentForm />
 
     return (
@@ -34,7 +50,7 @@ function Checkout() {
                             </Step>
                         ))}
                     </Stepper>
-                    {activeStep === steps.length ? <Confirmation /> : <Form />}
+                    {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
                 </Paper>
             </main>
         </>
